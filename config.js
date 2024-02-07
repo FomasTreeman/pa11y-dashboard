7b1f94ce827d57de41008f08f715a3c652620f43
FomasTreeman <tom@team-freeman.com>
2024-02-07 19:22:22 +0000

    db change config

--- config.js
+++ config.js
@@ -20,25 +20,25 @@ const jsonPath = `./config/${environment}.json`;
 const jsPath = `./config/${environment}.js`;
 
 if (fs.existsSync(jsonPath)) {
-	module.exports = require(jsonPath);
+  module.exports = require(jsonPath);
 } else if (fs.existsSync(jsPath)) {
-	module.exports = require(jsPath);
+  module.exports = require(jsPath);
 } else {
-	module.exports = {
-		port: Number(env('PORT', '4000')),
-		noindex: env('NOINDEX', 'true') === 'true',
-		readonly: env('READONLY', 'false') === 'true',
+  module.exports = {
+    port: Number(env('PORT', '4000')),
+    noindex: env('NOINDEX', 'true') === 'true',
+    readonly: env('READONLY', 'false') === 'true',
 
-		webservice: env('WEBSERVICE_URL', {
-			database: env('WEBSERVICE_DATABASE', 'mongodb://localhost/pa11y-webservice'),
-			host: env('WEBSERVICE_HOST', '0.0.0.0'),
-			port: Number(env('WEBSERVICE_PORT', '3000')),
-			cron: env('WEBSERVICE_CRON', false)
-		})
-	};
+    webservice: env('WEBSERVICE_URL', {
+      database: env('WEBSERVICE_DATABASE', 'mongodb+srv://admin:<password>@pa11ystorage.poomber.mongodb.net/?retryWrites=true&w=majority'),
+      host: env('WEBSERVICE_HOST', '0.0.0.0'),
+      port: Number(env('WEBSERVICE_PORT', '3000')),
+      cron: env('WEBSERVICE_CRON', false)
+    })
+  };
 }
 
 function env(name, defaultValue) {
-	const value = process.env[name];
-	return typeof value === 'string' ? value : defaultValue;
+  const value = process.env[name];
+  return typeof value === 'string' ? value : defaultValue;
 }
